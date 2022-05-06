0488074ac3e3e52264c2461cc30ee32adb197355
abdenacer1993 <abdenacer1993@gmail.com>
2022-05-06 20:54:21 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './Style.css';
-import logo from './logo.svg';
-import img from './image1.jpg';
+
+import img from './imageInSrc.jpg';
 import Video2 from './component/video/Video2';
 import Video from './component/video/Video';
 
@@ -13,14 +13,18 @@ function App() {
     <br />
     
     <br />
-    <img src={logo} className="App-logo" alt="logo" />
+    <div className='pad'>
+    <img src="./imageInPublic.svg" className="App-logo" alt="logo" />
     <br />
     <img src={img} className="img" alt="img" />
+    </div>
   </div>
+  <div className='pad'>
     <h2 className='title'> Video interne</h2>
     <Video />
     <h2 className='title'> Video lien externe</h2>
     <Video2 />
+    </div>
 
     </div>
   );
